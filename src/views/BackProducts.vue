0d6c0b1a9746ccda3b3d77a7a2155b82315187f4
chu <ayugioh2003@gmail.com>
2020-03-10 18:24:08 +0800

    Edit: 商品刪除 #12

--- src/views/BackProducts.vue
+++ src/views/BackProducts.vue
@@ -32,7 +32,9 @@
             <button class="btn btn-primary" @click="openModal(false, item)">
               編輯
             </button>
-            <button class="btn btn-danger">刪除</button>
+            <button class="btn btn-danger" @click="openDelModal(item)">
+              刪除
+            </button>
           </td>
         </tr>
       </tbody>
@@ -209,6 +211,50 @@
         </div>
       </div>
     </div>
+    <!-- del Modal -->
+    <div
+      class="modal fade"
+      id="delProductModal"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog" role="document">
+        <div class="modal-content border-0">
+          <div class="modal-header bg-danger text-white">
+            <h5 class="modal-title" id="exampleModalLabel">
+              <span> 刪除產品 </span>
+            </h5>
+            <button
+              type="button"
+              class="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            是否刪除
+            <strong class="text-danger">{{ tempProduct.title }}</strong> 商品
+            (刪除後將無法恢復)。
+          </div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-outline-secondary"
+              data-dismiss="modal"
+            >
+              取消
+            </button>
+            <button type="button" class="btn btn-danger" @click="delProduct">
+              確認刪除
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -239,7 +285,6 @@ export default {
         .then((products) => {
           vm.products = products;
           vm.isLoading = false;
-          console.log(vm);
           console.log(products);
         });
     },
@@ -296,6 +341,23 @@ export default {
       }
 
       $('#productModal').modal('show');
+    },
+    openDelModal(item) {
+      const vm = this;
+      vm.tempProduct = { ...item };
+      $('#delProductModal').modal('show');
+    },
+    delProduct() {
+      const vm = this;
+      const API = `${process.env.VUE_APP_API}/admin/product/${vm.tempProduct.id}`;
+      vm.isLoading = true;
+
+      this.$http.delete(API).then((response) => {
+        console.log(response);
+        $('#delProductModal').modal('hide');
+        vm.isLoading = false;
+        vm.getProductsA();
+      });
     },
   },
   created() {
