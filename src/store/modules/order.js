70e1fb646c8eb9fa644849f9b033a975c18e0ca4
chu <ayugioh2003@gmail.com>
2020-03-31 22:58:39 +0800

    Edit: BackOrderCheck 未繳費畫面 #17

--- src/store/modules/order.js
+++ src/store/modules/order.js
@@ -2,13 +2,33 @@ import axios from 'axios';
 
 export default {
   namespaced: true,
-
+  state: {
+    order: {},
+  },
+  getters: {
+    order: (state) => state.order,
+  },
+  mutations: {
+    GET_ORDER(state, payload) {
+      state.order = payload;
+    },
+  },
   actions: {
     createOrder(context, form) {
       const API = `${process.env.VUE_APP_API}/order`;
 
       return axios.post(API, { data: form }).then((res) => {
         console.log('訂單結帳結果', res);
+        return res;
+      });
+    },
+    getOrder(context, orderId) {
+      const API = `${process.env.VUE_APP_API}/order/${orderId}`;
+      context.commit('GET_ORDER', {});
+
+      axios.get(API).then((res) => {
+        console.log('取得訂單資料', res);
+        context.commit('GET_ORDER', res.data.order);
         return res;
       });
     },
--- src/views/BackOrderCheckout.vue
+++ src/views/BackOrderCheckout.vue
@@ -1,5 +1,76 @@
 <template>
-  <div>
-    check order {{ $route.params.id }}
+  <div class="row mt-5" v-if="order.create_at">
+    <div class="col-8 mx-auto">
+      <!-- check order {{ $route.params.id }} -->
+      <!-- 訂單產品資料 -->
+      <table class="table mb-3">
+        <thead>
+          <tr>
+            <th scope="col">品名</th>
+            <th scope="col">數量</th>
+            <th scope="col">單價</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in order.products" :key="item.id">
+            <th>{{ item.product.title }}</th>
+            <td>{{ item.qty }} {{ item.product.unit }}</td>
+            <td class="text-right">{{ item.product.price }}</td>
+          </tr>
+          <tr>
+            <td colspan="2" class="text-right">總計</td>
+            <td class="text-right">{{ order.total }}</td>
+          </tr>
+        </tbody>
+      </table>
+      <!-- 訂單買家資料 -->
+      <table class="table">
+        <tbody>
+          <tr>
+            <th scope="row" width="110">Email</th>
+            <td>{{ order.user.email }}</td>
+          </tr>
+          <tr>
+            <th scope="row">姓名</th>
+            <td>{{ order.user.name}}</td>
+          </tr>
+          <tr>
+            <th scope="row">收件人電話</th>
+            <td>{{ order.user.tel}}</td>
+          </tr>
+          <tr>
+            <th scope="row">收件人地址</th>
+            <td>{{ order.user.address}}</td>
+          </tr>
+          <tr>
+            <th scope="row">付款狀態</th>
+            <td>
+              <span class="text-danger" v-if="!order.is_paid">未付款</span>
+              <span class="text-success" v-else>已付款</span>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <!-- 付款按鈕 -->
+      <div class="text-right">
+        <button class="btn btn-danger btn-sm">確認付款去</button>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+  computed: {
+    ...mapGetters('order', ['order']),
+  },
+  methods: {
+    ...mapActions('order', ['getOrder']),
+  },
+  created() {
+    this.getOrder(this.$route.params.id);
+  },
+};
+</script>
