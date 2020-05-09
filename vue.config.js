41d6e36474cdc1afeeb7d2affbe1ed0f5db1b9e4
chu <ayugioh2003@gmail.com>
2020-05-10 00:23:41 +0800

    Fixed: remove console in production #33

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,19 @@
 const path = require('path');
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/psystore/' : '/',
+  // publicPath: './',
+
   chainWebpack: (config) => {
     const types = ['vue-modules', 'vue', 'normal-modules', 'normal'];
-    types.forEach((type) => addStyleResource(config.module.rule('scss').oneOf(type)));
+    types.forEach((type) =>
+      addStyleResource(config.module.rule('scss').oneOf(type)),
+    );
+  },
+  configureWebpack(config) {
+    if (process.env.NODE_ENV === 'production') {
+      config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true;
+    }
   },
 };
 
@@ -13,7 +23,10 @@ function addStyleResource(rule) {
     .loader('style-resources-loader')
     .options({
       patterns: [
-        path.resolve(__dirname, './node_modules/bootstrap/scss/_functions.scss'),
+        path.resolve(
+          __dirname,
+          './node_modules/bootstrap/scss/_functions.scss',
+        ),
         path.resolve(__dirname, './src/assets/helpers/_variables.scss'),
         path.resolve(__dirname, './node_modules/bootstrap/scss/_mixins.scss'),
       ],
