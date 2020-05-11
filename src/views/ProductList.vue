189409de1ead253439deb0f1dccf699a0d63ff5f
chu <ayugioh2003@gmail.com>
2020-05-11 17:44:21 +0800

    Edit: ProductList 引入 ProdcutCard

--- src/views/ProductList.vue
+++ src/views/ProductList.vue
@@ -35,85 +35,11 @@
           <div class="row">
             <!-- product cards -->
             <div
-              class="col-md-6 col-lg-4 mb-3 mb-md-5 "
+              class="col-md-6 col-lg-4 mb-3 mb-md-5"
               v-for="(item, index) in filterProducts"
               :key="item.id"
             >
-              <div class="card h-100 position-relative">
-                <div class="position-absolute" style="right: 0">
-                  <!-- 移出最愛 -->
-                  <button
-                    class="btn text-danger"
-                    @click="removeFavoritesItem(item)"
-                    v-if="
-                      favorites.map((product) => product.id).includes(item.id)
-                    "
-                  >
-                    <font-awesome-icon :icon="['fas', 'heart']" size="lg" />
-                  </button>
-                  <!-- 加到最愛 -->
-                  <button
-                    class="btn text-danger"
-                    @click="addToFavorites(item)"
-                    v-if="
-                      !favorites.map((product) => product.id).includes(item.id)
-                    "
-                  >
-                    <font-awesome-icon :icon="['far', 'heart']" size="lg" />
-                  </button>
-                </div>
-                <img
-                  :src="
-                    item.imageUrl || 'https://dummyimage.com/600x300/AAE.jpg'
-                  "
-                  class="card-img-top"
-                  alt="..."
-                />
-                <div class="card-body">
-                  <div class="d-flex justify-content-between align-item-start">
-                    <h5 class="card-title">{{ item.title }}</h5>
-                    <div>
-                      <span class="badge badge-pill badge-info">
-                        {{ item.category }}
-                      </span>
-                    </div>
-                  </div>
-                  <p class="card-text text-secondary">
-                    {{ item.description }}
-                  </p>
-                  <div class="d-flex justify-content-between">
-                    <p class="card-text text-decoration-line-through">
-                      <del>原價 {{ item.origin_price }}</del>
-                    </p>
-                    <p class="card-text h5 text-right">
-                      限時特價 {{ item.price }} 元
-                    </p>
-                  </div>
-                </div>
-                <div class="card-footer d-flex justify-content-between">
-                  <button
-                    class="btn btn-outline-secondary"
-                    @click="openProductDetail(item)"
-                  >
-                    查看更多
-                  </button>
-                  <button
-                    class="btn btn-outline-danger"
-                    @click="addtoCart({ product_id: item.id, qty: 1 })"
-                    @click.prevent="status.which_cartbtn_adding = index"
-                  >
-                    <font-awesome-icon
-                      icon="spinner"
-                      spin
-                      v-if="
-                        status.is_cartbtn_adding &&
-                          status.which_cartbtn_adding == index
-                      "
-                    />
-                    加到購物車
-                  </button>
-                </div>
-              </div>
+              <ProductCard :item="item" :index="index"></ProductCard>
             </div>
           </div>
         </div>
@@ -124,8 +50,12 @@
 
 <script>
 import { mapGetters, mapActions } from 'vuex';
+import ProductCard from '@/components/ProductCard.vue';
 
 export default {
+  components: {
+    ProductCard,
+  },
   metaInfo() {
     return {
       title: `${this.$route.params.category}-商品列表 | PsyStore`,
